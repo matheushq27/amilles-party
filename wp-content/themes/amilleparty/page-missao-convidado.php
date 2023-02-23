4a9a1f534619e593ff5d75672307b5dd2edb2c2c
matheushq27 <matheus.e.arruda@gmail.com>
2023-02-23 18:28:34 -0400

    Adição da página missão convidado

--- wp-content/themes/amilleparty/page-missao-convidado.php
+++ wp-content/themes/amilleparty/page-missao-convidado.php
@@ -1,16 +1,36 @@
 <?php get_header(); ?>
 <section>
-    <div class="bg-banner-guest-mission" style="background-image: url(<?= get_template_directory_uri().'/assets/img/2.png' ?>);">
+    <div class="bg-banner-guest-mission mb-5" style="background-image: url(<?= get_template_directory_uri().'/assets/img/2.png' ?>);">
         <h1>Missão Convidado</h1>
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 mb-3">
+            <h2 class="amille-party-color-primary">Olá amigos e familiares!</h2>    
+            <p>Está chegando o grande momento de celebrar o sonho de uma data Especial! Amille 15 Anos!</p>
+            <p>Queremos que todos se divirtam e aproveitem ao máximo o momento, por isso, vamos passar algumas dicas: 
+</p>
+            <ul>
+                <li>Confirme sua presença ✅.</li>
+                <li>Se você irá ou não, nos informe aqui pelo site.</li>
+                <li>Horário ⏰:</li>
+            </ul>
+                <p>
+                  Se não quiser perder a entrada da Debutante, A valsa e Abertura da Pista, chegue na hora combinada: a chegada dos convidados será às 21h. Não se atrase!
+                </p>
 
-            </div>
-            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
-                <h1></h1>
-                <p></p>
+                <h2 class="amille-party-color-primary">Cores a serem evitadas</h2>
+                <p>Em um dia especial, gostaríamos muito que nossa debutante tenha destaque, então se puderem evitar as cores da Decoração – Paleta de Roxo ao Azul Marinho, seria muito legal! </p>
+
+                <h2 class="amille-party-color-primary">Câmeras são permitidas 🤳! </h2>
+                <p>Apesar de termos um fotógrafo profissional registrando tudo, queremos ver a festa pelos seus olhos! Registre todos os momentos, vamos amar repostar e ver depois. Nossos instagrans são: @mille_leal_ , @cibelleleal @francisco.dinizcf
+                </p>
+                <div><strong class="amille-party-color-primary">A hashtag niver é #amillefaz15
+</strong></div>
+<p>Tudo foi pensado e projetado com muito carinho. Queremos de todo coração compartilhar esse momento incrível com todos vocês.
+</p>
+
+<p>Nos vemos no dia 01.04</p>
             </div>
         </div>
     </div>
